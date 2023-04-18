b0f4330995cd7675febf25194cdbf088dd81719b
Sudipto Dutta <sudiptodutta_1992@outlook.com>
2023-04-18 12:40:53 +0530

    51. Injecting services into components

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AccountService {
-  baseUrl = 'https://localhost:5000/api/';
+  baseUrl = 'https://localhost:5001/api/';
 
   constructor(private http: HttpClient) { }
 
