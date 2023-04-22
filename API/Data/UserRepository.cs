f219aa5ccd0d9ecd19412efabad72febbb421818
Sudipto Dutta <sudiptodutta_1992@outlook.com>
2023-04-23 01:06:13 +0530

    92. Updating the users controller

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -24,7 +24,9 @@ namespace API.Data
 
         public async Task<IEnumerable<AppUser>> GetUsersAsync()
         {
-            return await _context.Users.ToListAsync();
+            return await _context.Users
+                .Include(p => p.Photos)
+                .ToListAsync();
         }
 
         public async Task<AppUser> GetUserByIdAsync(int id)
@@ -34,7 +36,9 @@ namespace API.Data
 
         public async Task<AppUser> GetUserByUserNameAsync(string username)
         {
-            return await _context.Users.SingleOrDefaultAsync(x => x.UserName == username);
+            return await _context.Users
+                .Include(p => p.Photos)
+                .SingleOrDefaultAsync(x => x.UserName == username);
         }
     }
 }
