5cce135bb33d288a572fdcb24fd357d350acd132
Sudipto Dutta <sudiptodutta_1992@outlook.com>
2023-04-22 12:13:55 +0530

    76. Exception handling middleware

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Extensions;
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,12 +13,11 @@ builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
-if(builder.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-}
+
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
+
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.UseAuthentication();
