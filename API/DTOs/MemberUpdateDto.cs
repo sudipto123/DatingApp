60be0b3abd64be3e42f00f99eeafa143c9a2ed59
Sudipto Dutta <sudiptodutta_1992@outlook.com>
2023-04-24 17:49:54 +0530

    118. Persisting the changes in the API

---
+++ API/DTOs/MemberUpdateDto.cs
@@ -0,0 +1,11 @@
+namespace API.DTOs
+{
+    public class MemberUpdateDto
+    {
+        public string Inntroduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
