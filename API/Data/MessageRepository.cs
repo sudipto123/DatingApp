06594886ff8ac81159de62516c3d906897f2fedf
Sudipto Dutta <sudiptodutta_1992@outlook.com>
2023-05-12 18:28:58 +0530

    194. Sending messages

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -61,7 +61,7 @@ namespace API.Data
                     m.RecipientUsername == recipentUserName &&
                     m.SenderUsername == currentUserName
                 )
-                .OrderByDescending(m => m.MessageSent)
+                .OrderBy(m => m.MessageSent)
                 .ToListAsync();
 
             var unreadMessages = messages.Where(m => m.DateRead == null && 
--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Message } from 'src/app/_models/message';
 import { MembersService } from 'src/app/_services/members.service';
 import { MessageService } from 'src/app/_services/message.service';
@@ -9,13 +10,23 @@ import { MessageService } from 'src/app/_services/message.service';
   styleUrls: ['./member-messages.component.css']
 })
 export class MemberMessagesComponent implements OnInit {
+  @ViewChild('messageForm') messageForm?: NgForm
   @Input() username?: string;
   @Input() messages: Message[] = [];
+  messageContent = '';
 
   constructor(private messageService: MessageService) { }
 
   ngOnInit(): void {
   }
 
-  
+  sendMessage(){
+    if(!this.username) return;
+    this.messageService.sendMessage(this.username, this.messageContent).subscribe({
+      next: message => {
+        this.messages.push(message);
+        this.messageForm?.reset()
+      }
+    })
+  }
 }
