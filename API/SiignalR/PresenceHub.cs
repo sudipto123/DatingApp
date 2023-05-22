fa8beed17d0514560741c7195cdd9bfe54e7c56b
Sudipto Dutta <sudiptodutta_1992@outlook.com>
2023-05-22 11:21:42 +0530

    220. Adding a presence hub

---
+++ API/SiignalR/PresenceHub.cs
@@ -0,0 +1,20 @@
+using API.Extensions;
+using Microsoft.AspNetCore.SignalR;
+
+namespace API.SiignalR
+{
+    public class PresenceHub : Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.Others.SendAsync("UserIsOnline", Context.User.GetUserName());
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Clients.Others.SendAsync("UserIsOffline", Context.User.GetUserName());
+
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
