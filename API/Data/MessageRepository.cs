6f2ed1199c608cb4017f08d49ebdee7e00f14009
Sudipto Dutta <sudiptodutta_1992@outlook.com>
2023-05-26 17:25:13 +0530

    240. Optimizing queries part one

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -78,9 +78,7 @@ namespace API.Data
 
         public async Task<IEnumerable<MessageDto>> GetMessageThread(string currentUserName, string recipentUserName)
         {
-            var messages = await _context.Messages
-                .Include(u => u.Sender).ThenInclude(p => p.Photos)
-                .Include(u => u.Recipient).ThenInclude(p => p.Photos)
+            var query = _context.Messages
                 .Where(
                     m => m.RecipientUsername == currentUserName && m.RecipientDeleted == false &&
                     m.SenderUsername == recipentUserName ||
@@ -88,9 +86,9 @@ namespace API.Data
                     m.SenderUsername == currentUserName
                 )
                 .OrderBy(m => m.MessageSent)
-                .ToListAsync();
+                .AsQueryable();
 
-            var unreadMessages = messages.Where(m => m.DateRead == null && 
+            var unreadMessages = query.Where(m => m.DateRead == null && 
                 m.RecipientUsername == currentUserName).ToList();
 
             if(unreadMessages.Any())
@@ -101,7 +99,7 @@ namespace API.Data
                 }
             }
 
-            return _mapper.Map<IEnumerable<MessageDto>>(messages);
+            return await query.ProjectTo<MessageDto>(_mapper.ConfigurationProvider).ToListAsync();
         }
 
         public void RemoveConnection(Connection connection)
