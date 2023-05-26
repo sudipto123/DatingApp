b1b8825db216de4b4784f68d8a5ab611daaa4528
Sudipto Dutta <sudiptodutta_1992@outlook.com>
2023-05-26 20:29:12 +0530

    244. Tidying up the member message component

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -13,7 +13,7 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public accountService: AccountService, private router: Router, 
+  constructor(public accountService: AccountService, private router: Router,
     private toastr: ToastrService) { }
 
   ngOnInit(): void {
@@ -21,13 +21,17 @@ export class NavComponent implements OnInit {
 
   login() {
     this.accountService.login(this.model).subscribe({
-      next: _ => this.router.navigateByUrl('/members')
+      next: _ => {
+        this.router.navigateByUrl('/members');
+        this.model = {};
+      }
     })
   }
 
   logout() {
     this.accountService.logout();
     this.router.navigateByUrl('/');
+    window.location.replace('/path');
   }
 
 }
